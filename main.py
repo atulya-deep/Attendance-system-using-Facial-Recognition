b5792e91d80d79c9b932b80d3c07e10fdd6aa89d
ATULYA DEEP <83969166+atulya-deep@users.noreply.github.com>
2022-09-09 23:57:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,32 +37,4 @@ def markAttendance(name):
 encodeListKnown = findencoding(images)
 print('Encoding Complete')
 
-cap = cv2.VideoCapture(0)
-while True:
-    success, img = cap.read()
-    imgs = cv2.resize(img,(0,0),None,0.25,0.25)
-    imgs = cv2.cvtColor(imgs, cv2.COLOR_BGR2RGB)
-
-    facesCurFrame = face_recognition.face_locations(imgs)
-    encodesCurFrame = face_recognition.face_encodings(imgs,facesCurFrame)
-
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-        matches = face_recognition.compare_faces( encodeListKnown,encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
-        print(faceDis)
-        matchIndex=np.argmin(faceDis)
-
-        if matches[matchIndex]:
-            name = classname[matchIndex].upper()
-            print(name)
-            y1,x2,y2,x1 = faceLoc
-            y1, x2, y2, x1 = y1 *4 ,x2 *4 ,y2 * 4 ,x1 * 4
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0, 255 ,0),2)
-            cv2.rectangle(img,(x1,y2 - 35),(x2,y2), (0,255, 0),cv2.FILLED)
-            cv2.putText(img,name,(x1 + 6,y2 - 6), cv2.FONT_HERSHEY_COMPLEX,1,(255, 255, 255), 2)
-            markAttendance(name)
-
-    cv2.imshow('Webcam',img)
-    cv2.waitKey(1)
-
 
